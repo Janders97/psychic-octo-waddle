eee590752a42a62ea48f1676e1292d3465b466e8
Janders97 <jake.anderson008@yahoo.com>
2026-05-28 00:11:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const WEB_APP_URL = "PASTE_YOUR_DEPLOYED_WEB_APP_URL_HERE";
+const WEB_APP_URL = "https://script.google.com/a/macros/colorado.edu/s/AKfycbxaAxhKUIFgEDNw5hj7BY6--u3_eBc94mEhddaHCfdktHgcSOy259OZhqUg_pxn3xhD/exec";
 const PICKS_PUBLIC = false;
 const ENTRY_SHEET_URL = "PASTE_YOUR_GOOGLE_SHEET_URL_HERE";
 
