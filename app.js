dd79345f486756eece96e830823e48a7f71bb010
Janders97 <jake.anderson008@yahoo.com>
2026-06-18 00:17:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // ─── CONFIG ──────────────────────────────────────────────────────────────────
 
-const WEB_APP_URL    = "https://script.google.com/macros/s/AKfycbxfghiDreRM0OittBv8bKi4ak25KlMXhzx9Cq_h-UA58_qcVbBcv3Hw7mQ-WeyoUW_N/exec";
+const WEB_APP_URL    = "https://script.google.com/macros/s/AKfycbyrMe3yLqqzGhkdC7HcB7HHrkgF6q1TQDIXc4q9mp_K-mgQ0CBR0wlYViLh3rTWMITM/exec";
 const ENTRY_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSe6zAHK_tEozTJuD1ALQwpPjXFdB1jwwhkRT49sfI8YPoiqTw/viewform";
 
 // ─── STATE ───────────────────────────────────────────────────────────────────
